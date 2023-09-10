c99586f685cb57e7a482475bb7434221486328fd
Pramitha Jayasooriya <lpramithamj@gmail.com>
2023-09-10 08:19:47 +0530

    add 3.1 update

---
+++ retrieve_advance_queries.sql
@@ -0,0 +1,20 @@
+-- Data retrieve and advance queries
+
+-- Table Retrieves
+SELECT * FROM criminal_DBMS.ARRESTED_CRIMINALS;
+SELECT * FROM criminal_DBMS.CASES;
+SELECT * FROM criminal_DBMS.CloseContacts;
+SELECT * FROM criminal_DBMS.Crime;
+SELECT * FROM criminal_DBMS.CrimeWitnessVictim;
+SELECT * FROM criminal_DBMS.CRIMINAL;
+SELECT * FROM criminal_DBMS.CRIMINAL_ADDRESS;
+SELECT * FROM criminal_DBMS.CRIMINAL_ADDRESS_RELATION;
+SELECT * FROM criminal_DBMS.CRIMINAL_FINGERPRINT;
+SELECT * FROM criminal_DBMS.FINGERPRINT;
+SELECT * FROM criminal_DBMS.JAIL;
+SELECT * FROM criminal_DBMS.REHABILITATION;
+SELECT * FROM criminal_DBMS.Victim;
+SELECT * FROM criminal_DBMS.VictimCloseContacts;
+SELECT * FROM criminal_DBMS.VictimContact;
+SELECT * FROM criminal_DBMS.VictimCrime;
+SELECT * FROM criminal_DBMS.Witness;
